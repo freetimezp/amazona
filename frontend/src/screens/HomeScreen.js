22b292d0ccff967d9ddc3d65a30509a1bbfdf8ac
freetimezp <kraftukrnet@ukr.net>
2022-08-12 17:23:56 +0300

    10. Amazona. Add rating to product.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Rating from "../components/rating.js";
 
 const HomeScreen = {
     render: async () => {
@@ -25,6 +26,12 @@ const HomeScreen = {
                             <a href="/#/product/${product._id}">
                                 ${product.name}
                             </a>
+                        </div>
+                        <div class="product-rating">
+                            ${Rating.render({
+                                value: product.rating,
+                                text: `${product.numReviews} reviews`,
+                            })}
                         </div>
                         <div class="product-brand">
                             ${product.brand}
